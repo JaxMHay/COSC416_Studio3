915d324df34b50db7d1c87dd8601de5957642655
JaxMHay <jaxonmhay@gmail.com>
2025-03-01 23:54:08 -0800

    Imported coin visual asset and implemented coin spin

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+
+    void Update()
+    {
+        //rotate coin
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
